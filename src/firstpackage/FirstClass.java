6e13bfc9a66ed537302790561a359217d7f62005
osman <osmnndm@protonmail.com>
2020-06-20 20:35:09 +0200

    ikinci commit local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -1,5 +1,13 @@
 package firstpackage;
 
 public class FirstClass {
+public static void main(String[] args) {
+		
+		System.out.println("first commit from local repository");
+		System.out.println("second commit from local repository");
+		
+	
+	
+	}
 
 }
