1ee2a58455b72201eae11242c4aaa387fb398612
osmnndm <67065739+osmnndm@users.noreply.github.com>
2020-06-20 21:16:44 +0200

    Update FirstClass.java

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,6 +6,7 @@ public static void main(String[] args) {
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
 		System.out.println("thirt comit from local repository");
+		System.out.println("first commit from remote repository");
 		
 	
 	
