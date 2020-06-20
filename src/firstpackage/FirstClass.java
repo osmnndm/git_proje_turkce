46efb310bed3858b5a85d4fad2883d4eb23a5745
osman <osmnndm@protonmail.com>
2020-06-20 20:45:55 +0200

    üçüncü commit local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 		
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
+		System.out.println("thirt comit from local repository");
 		
 	
 	
