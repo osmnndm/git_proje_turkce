4c91524186b8649ded99a750f361899994d9fd14
osmnndm <67065739+osmnndm@users.noreply.github.com>
2020-06-20 21:46:17 +0200

    Update FirstClass.java
    ARTIK ÇÖZÜM ARIYORUZ

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,7 +3,7 @@ package firstpackage;
 public class FirstClass {
 public static void main(String[] args) {
 		
-		System.out.println("first commit from local repository");
+		System.out.println("BURADA DEĞİŞİKLİK YAPTIM from local repository");
 		System.out.println("second commit from local repository");
 		System.out.println("thirt comit from local repository");
 		System.out.println("first commit from remote repository");
